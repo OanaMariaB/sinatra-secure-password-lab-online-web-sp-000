1f103ceb85ce6e3b9a868692487624691001ebb3
Oana Berentan <oana.berentan@yahoo.com>
2020-01-08 20:51:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    if params[:username] == "" || params[:password] == ""
+        redirect "/failure"
+      else
+        redirect "/account"
   end
 
   get "/failure" do
